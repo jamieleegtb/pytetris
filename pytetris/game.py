14c06b587e327fb1ab9e45584a47262ba47ee57e
raymondberg <dev@raymondberg.com>
2016-09-10 15:41:27 -0500

    refactor - isolate key reading logic in game

--- pytetris/game.py
+++ pytetris/game.py
@@ -5,6 +5,7 @@ from sys import exit
 
 class Game:
     def __init__(self, **kwargs):
+        self.is_running = False
         self.is_loaded = True
         pygame.mixer.pre_init(44100, -16, 2, 2048)
         pygame.init()
@@ -18,14 +19,14 @@ class Game:
         pygame.quit()
 
     def run(self):
-        running = True
+        self.is_running = True
         pygame.display.set_caption("Pytris")
         clock = pygame.time.Clock()
         pygame.mixer.music.set_volume(0.3)
         load_music('bg_music.ogg')
         board = GameBoard()
         board.initialize(pygame.time.get_ticks())
-        while running:
+        while self.is_running:
             if not pygame.mixer.music.get_busy():
                     pygame.mixer.music.play()
             for event in pygame.event.get():
@@ -35,48 +36,59 @@ class Game:
                         for buttons in board.buttons:
                             buttons.check_click(position)
                 if event.type == pygame.QUIT:
-                    running = False
+                    self.is_running = False
                 if board.paused:
                     break
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        running = False
-                    if event.key == pygame.K_g:
-                        board.toggle_grid()
-                        board.update(pygame.time.get_ticks())
-                    if event.key == pygame.K_s:
-                        board.slow_time = not board.slow_time
-                    if event.key == pygame.K_LEFT:
-                        if board.last_strafe == 0:
-                            board.current_gp.move_left()
-                            board.last_strafe = (pygame.time.get_ticks()+250)
-                            board.key_left_flag = True
-                    if event.key == pygame.K_RIGHT:
-                        if board.last_strafe == 0:
-                            board.current_gp.move_right()
-                            board.last_strafe = (pygame.time.get_ticks()+250)
-                            board.key_right_flag = True
-                    if event.key == pygame.K_DOWN:
-                        board.key_down_flag = True
-                    if event.key == pygame.K_UP:
-                        board.current_gp.rotate()
+                    self.__handle_key_down(event.key, board)
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_DOWN:
-                        board.key_down_flag = False
-                    if event.key == pygame.K_LEFT:
-                        board.key_left_flag = False
-                        board.last_strafe = 0
-                    if event.key == pygame.K_RIGHT:
-                        board.key_right_flag = False
-                        board.last_strafe = 0
+                    self.__handle_key_up(event.key, board)
+            self.__update_board(board)
+
+
+    def __update_board(self, board):
+        board.update(pygame.time.get_ticks())
+        self.screen.fill((175,175,175))
+        board.draw(self.screen)
+        if board.game_over:
+            board.puase = True
+            x = self.screen_width
+            y = self.screen_height
+            xx = game_over_image.get_width()
+            yy = game_over_image.get_height()
+            self.screen.blit(game_over_image, ((x - xx)/2, (y - yy)/2))
+        pygame.display.flip()
+
+
+    def __handle_key_down(self, key, board):
+        if key == pygame.K_ESCAPE:
+            self.is_running = False
+        if key == pygame.K_g:
+            board.toggle_grid()
             board.update(pygame.time.get_ticks())
-            self.screen.fill((175,175,175))
-            board.draw(self.screen)
-            if board.game_over:
-                board.puase = True
-                x = self.screen_width
-                y = self.screen_height
-                xx = game_over_image.get_width()
-                yy = game_over_image.get_height()
-                self.screen.blit(game_over_image, ((x - xx)/2, (y - yy)/2))
-            pygame.display.flip()
+        if key == pygame.K_s:
+            board.slow_time = not board.slow_time
+        if key == pygame.K_LEFT:
+            if board.last_strafe == 0:
+                board.current_gp.move_left()
+                board.last_strafe = (pygame.time.get_ticks()+250)
+                board.key_left_flag = True
+        if key == pygame.K_RIGHT:
+            if board.last_strafe == 0:
+                board.current_gp.move_right()
+                board.last_strafe = (pygame.time.get_ticks()+250)
+                board.key_right_flag = True
+        if key == pygame.K_DOWN:
+            board.key_down_flag = True
+        if key == pygame.K_UP:
+            board.current_gp.rotate()
+
+    def __handle_key_up(self, key, board):
+        if key == pygame.K_DOWN:
+            board.key_down_flag = False
+        if key == pygame.K_LEFT:
+            board.key_left_flag = False
+            board.last_strafe = 0
+        if key == pygame.K_RIGHT:
+            board.key_right_flag = False
+            board.last_strafe = 0
