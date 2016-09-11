7163bfacf279143173f10076568b2a481066360b
raymondberg <dev@raymondberg.com>
2016-09-10 23:37:17 -0500

    feature #4 - add gamepad support for the buffalo usb gamepad

--- pytetris/game.py
+++ pytetris/game.py
@@ -20,8 +20,11 @@ class Game:
             options["mixer_buffer_size"]
         )
         pygame.init()
+        pygame.joystick.init()
 
         load_music(options["music_file"])
+        [pygame.joystick.Joystick(x).init() for x in range(pygame.joystick.get_count())]
+
         pygame.mixer.music.set_volume(options["mixer_volume_music"])
 
         pygame.display.set_caption("Pytris")
@@ -32,6 +35,7 @@ class Game:
     def __del__(self):
         self.is_loaded = False
         del self.board
+        pygame.joystick.quit()
         pygame.quit()
 
     def run(self):
@@ -49,12 +53,47 @@ class Game:
                             buttons.check_click(position)
                 if event.type == pygame.QUIT:
                     self.is_running = False
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        self.is_running = False
-                    else:
-                        self.board.register_game_key_down(event.key)
-                if event.type == pygame.KEYUP:
-                    self.board.register_game_key_up(event.key)
+                else:
+                    self.__handle_game_event(event)
+
             self.board.update()
             pygame.display.flip()
+
+    def __handle_game_event(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                self.is_running = False
+            else:
+                self.board.register_game_key_down(event.key)
+        if event.type == pygame.JOYAXISMOTION:
+            if event.axis == 0:
+                if event.value == 0.0:
+                    self.board.register_game_key_up(pygame.K_RIGHT)
+                    self.board.register_game_key_up(pygame.K_LEFT)
+                elif abs(event.value - 1.0) < 0.001:
+                    self.board.register_game_key_down(pygame.K_RIGHT)
+                elif abs(event.value + 1.0) < 0.001:
+                    self.board.register_game_key_down(pygame.K_LEFT)
+
+            if event.axis == 1:
+                if event.value == 0.0:
+                    self.board.register_game_key_up(pygame.K_UP)
+                    self.board.register_game_key_up(pygame.K_DOWN)
+                elif abs(event.value - 1.0) < 0.001:
+                    self.board.register_game_key_down(pygame.K_DOWN)
+                elif abs(event.value + 1.0) < 0.001:
+                    self.board.register_game_key_down(pygame.K_UP)
+
+        if event.type == pygame.JOYBUTTONDOWN:
+            if event.button == 1:
+                self.board.register_game_key_down(pygame.K_UP)
+            elif event.button == 7:
+                self.board.register_game_key_down(pygame.K_p)
+            elif event.button == 6:
+                self.is_running = False
+
+        if event.type == pygame.JOYBUTTONUP:
+            if event.button == 1:
+                self.board.register_game_key_up(pygame.K_UP)
+        if event.type == pygame.KEYUP:
+            self.board.register_game_key_up(event.key)
